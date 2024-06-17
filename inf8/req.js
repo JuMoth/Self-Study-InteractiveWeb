4249929793576b82252393b93d19641f6625ab85
JuMoth <tkdlqjqkd@gmail.com>
2024-06-17 18:34:56 +0900

    Feat: requestAnimationFrame

--- inf8/req.js
+++ inf8/req.js
@@ -1,16 +1,25 @@
 let timeId;
+let n = 0;
 const btn = document.querySelector('.btn');
 
 function sample() {
-    console.log('sample!');
+    n++;
+    console.log(n);
+    if(n>200) {
+        return;
+    }
+
+    timeId = requestAnimationFrame(sample);
 }
 
+sample();
+
 // timeId = setTimeout(sample, 3000);
-timeId = setInterval(sample, 1000/60);
+// timeId = setInterval(sample, 1000/60);
 
 // btn.addEventListener('click', () => {
 //     clearTimeout(timeId);
 // });
 btn.addEventListener('click', () => {
-    clearInterval(timeId);
+    cancelAnimationFrame(timeId);
 });
