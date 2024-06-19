1e4e3844e2045828b8f9160cd51b36e64fc7f5f9
JuMoth <tkdlqjqkd@gmail.com>
2024-06-19 22:02:37 +0900

    Feat: apply resize by screen size

--- inf9/js/wall3d.js
+++ inf9/js/wall3d.js
@@ -1,9 +1,16 @@
 (function() {
     const houseElem = document.querySelector('.house');
-    let maxScrollValue = document.body.offsetHeight-window.innerHeight;
+    let maxScrollValue = 0;
+
+    function resizeHandler() {
+        maxScrollValue = document.body.offsetHeight-window.innerHeight;
+    }
 
     window.addEventListener('scroll', () => {
         const zMove = scrollY / maxScrollValue * 980 - 490;
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
-    })
+    });
+
+    window.addEventListener('resize', resizeHandler);
+    resizeHandler();
 })();
