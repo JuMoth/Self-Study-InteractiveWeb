0b60de6aab6c167b317cd6a4c8fda8760d156d86
JuMoth <tkdlqjqkd@gmail.com>
2024-06-20 17:59:42 +0900

    Feat: setting location where character is created

--- inf9/js/Character.js
+++ inf9/js/Character.js
@@ -69,104 +69,104 @@ function Character(info) {
     // this.init();
 }
 
-// Character.prototype = {
-//     constructor: Character,
-//     init: function () {
-//         const self = this;
+Character.prototype = {
+    constructor: Character,
+    // init: function () {
+    //     const self = this;
 
-//         window.addEventListener('scroll', function () {
-//             clearTimeout(self.scrollState);
+    //     window.addEventListener('scroll', function () {
+    //         clearTimeout(self.scrollState);
 
-//             if (!self.scrollState) {
-//                 self.mainElem.classList.add('running');
-//             }
+    //         if (!self.scrollState) {
+    //             self.mainElem.classList.add('running');
+    //         }
 
-//             self.scrollState = setTimeout(function () {
-//                 self.scrollState = false;
-//                 self.mainElem.classList.remove('running');
-//             }, 500);
+    //         self.scrollState = setTimeout(function () {
+    //             self.scrollState = false;
+    //             self.mainElem.classList.remove('running');
+    //         }, 500);
 
-//             // 이전 스크롤 위치와 현재 스크롤 위치를 비교
-//             if (self.lastScrollTop > pageYOffset) {
-//                 // 이전 스크롤 위치가 크다면: 스크롤 올림
-//                 self.mainElem.setAttribute('data-direction', 'backward');
-//             } else {
-//                 // 현재 스크롤 위치가 크다면: 스크롤 내림
-//                 self.mainElem.setAttribute('data-direction', 'forward');
-//             }
+    //         // 이전 스크롤 위치와 현재 스크롤 위치를 비교
+    //         if (self.lastScrollTop > scrollY) {
+    //             // 이전 스크롤 위치가 크다면: 스크롤 올림
+    //             self.mainElem.setAttribute('data-direction', 'backward');
+    //         } else {
+    //             // 현재 스크롤 위치가 크다면: 스크롤 내림
+    //             self.mainElem.setAttribute('data-direction', 'forward');
+    //         }
 
-//             self.lastScrollTop = pageYOffset;
-//         });
+    //         self.lastScrollTop = scrollY;
+    //     });
 
-//         window.addEventListener('keydown', function (e) {
-//             if (self.runningState) return;
+    //     window.addEventListener('keydown', function (e) {
+    //         if (self.runningState) return;
 
-//             if (e.keyCode == 37) {
-//                 // 왼쪽
-//                 self.direction = 'left';
-//                 self.mainElem.setAttribute('data-direction', 'left');
-//                 self.mainElem.classList.add('running');
-//                 self.run(self);
-//                 // self.run(); // bind를 사용한 방법
-//                 self.runningState = true;
-//             } else if (e.keyCode == 39) {
-//                 // 오른쪽
-//                 self.direction = 'right';
-//                 self.mainElem.setAttribute('data-direction', 'right');
-//                 self.mainElem.classList.add('running');
-//                 self.run(self);
-//                 // self.run(); // bind를 사용한 방법
-//                 self.runningState = true;
-//             }
-//         });
+    //         if (e.KeyboardEvent.code == 37) {
+    //             // 왼쪽
+    //             self.direction = 'left';
+    //             self.mainElem.setAttribute('data-direction', 'left');
+    //             self.mainElem.classList.add('running');
+    //             self.run(self);
+    //             // self.run(); // bind를 사용한 방법
+    //             self.runningState = true;
+    //         } else if (e.KeyboardEvent.code == 39) {
+    //             // 오른쪽
+    //             self.direction = 'right';
+    //             self.mainElem.setAttribute('data-direction', 'right');
+    //             self.mainElem.classList.add('running');
+    //             self.run(self);
+    //             // self.run(); // bind를 사용한 방법
+    //             self.runningState = true;
+    //         }
+    //     });
 
-//         window.addEventListener('keyup', function (e) {
-//             self.mainElem.classList.remove('running');
-//             cancelAnimationFrame(self.rafId);
-//             self.runningState = false;
-//         });
-//     },
-//     run: function (self) {
-//         if (self.direction == 'left') {
-//             self.xPos -= self.speed;
-//         } else if (self.direction == 'right') {
-//             self.xPos += self.speed;
-//         }
+    //     window.addEventListener('keyup', function (e) {
+    //         self.mainElem.classList.remove('running');
+    //         cancelAnimationFrame(self.rafId);
+    //         self.runningState = false;
+    //     });
+    // },
+    // run: function (self) {
+    //     if (self.direction == 'left') {
+    //         self.xPos -= self.speed;
+    //     } else if (self.direction == 'right') {
+    //         self.xPos += self.speed;
+    //     }
 
-//         if (self.xPos < 2) {
-//             self.xPos = 2;
-//         }
+    //     if (self.xPos < 2) {
+    //         self.xPos = 2;
+    //     }
 
-//         if (self.xPos > 88) {
-//             self.xPos = 88;
-//         }
+    //     if (self.xPos > 88) {
+    //         self.xPos = 88;
+    //     }
 
-//         self.mainElem.style.left = self.xPos + '%';
+    //     self.mainElem.style.left = self.xPos + '%';
 
-//         self.rafId = requestAnimationFrame(function () {
-//             self.run(self);
-//         });
-//     }
-//     // bind를 사용한 방법
-//     // run: function () {
-//     //     const self = this;
-//     //
-//     //     if (self.direction == 'left') {
-//     //         self.xPos -= self.speed;
-//     //     } else if (self.direction == 'right') {
-//     //         self.xPos += self.speed;
-//     //     }
-//     //
-//     //     if (self.xPos < 2) {
-//     //         self.xPos = 2;
-//     //     }
-//     //
-//     //     if (self.xPos > 88) {
-//     //         self.xPos = 88;
-//     //     }
-//     //
-//     //     self.mainElem.style.left = self.xPos + '%';
-//     //
-//     //     self.rafId = requestAnimationFrame(self.run.bind(self));
-//     // }
-// };
+    //     self.rafId = requestAnimationFrame(function () {
+    //         self.run(self);
+    //     });
+    // }
+    // bind를 사용한 방법
+    // run: function () {
+    //     const self = this;
+    //
+    //     if (self.direction == 'left') {
+    //         self.xPos -= self.speed;
+    //     } else if (self.direction == 'right') {
+    //         self.xPos += self.speed;
+    //     }
+    //
+    //     if (self.xPos < 2) {
+    //         self.xPos = 2;
+    //     }
+    //
+    //     if (self.xPos > 88) {
+    //         self.xPos = 88;
+    //     }
+    //
+    //     self.mainElem.style.left = self.xPos + '%';
+    //
+    //     self.rafId = requestAnimationFrame(self.run.bind(self));
+    // }
+};
