cb740d0ebfdcc4ea2cc9093241c04d8f67b3add1
JuMoth <tkdlqjqkd@gmail.com>
2024-06-20 22:50:34 +0900

    Feat: add character change feature

--- inf9/js/wall3d.js
+++ inf9/js/wall3d.js
@@ -2,6 +2,7 @@
     const stageElem = document.querySelector('.stage');
     const houseElem = document.querySelector('.house');
     const barElem = document.querySelector('.progress-bar');
+    const selectCharacterElem = document.querySelector('.select-character');
     const mousePos = { x: 0, y: 0 };
     let maxScrollValue = 0;
 
@@ -22,7 +23,7 @@
         // 외워서 사용하기
         mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
         mousePos.y = 1 - (e.clientY / window.innerHeight) * 2;
-        stageElem.style.transform = 'rotateX(' + (mousePos.y * 10) + 'deg) rotateY(' + (mousePos.x * 10) + 'deg)';
+        stageElem.style.transform = 'rotateX(' + (mousePos.y * 10) + 'deg) rotateY(' + (mousePos.x * 5) + 'deg)';
     });
 
     window.addEventListener('resize', resizeHandler);
@@ -32,7 +33,12 @@
             xPos: e.clientX / window.innerWidth * 100,
             speed: Math.random() * 0.5 + 0.2
         });
-    })
+    });
+
+    selectCharacterElem.addEventListener('click', (e) => {
+        const value = e.target.getAttribute('data-char');
+        document.body.setAttribute('data-char', value);
+    });
 
     resizeHandler();
 })();
